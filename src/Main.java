9357e71d255b2289b53e21c7681f2012b8fb6aa4
Silas Onyango <silas.onyango93@gmail.com>
2022-02-12 14:17:06 +0300

    doubly linked list

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,92 @@
+import doubly_linked_list.DoublyLinkedList;
+
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //iterateString("rararararyy");
+
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.insertAtBeginningOfList(8);
+        doublyLinkedList.insertAtBeginningOfList(6);
+        doublyLinkedList.insertAtBeginningOfList(3);
+        doublyLinkedList.insertAtBeginningOfList(4);
+        doublyLinkedList.insertAtEndOfList(5);
+        doublyLinkedList.insertAtEndOfList(0);
+        doublyLinkedList.insertAtEndOfList(1);
+        doublyLinkedList.findNodeWithParticularValue(5);
+    }
+
+    public static int iterateString(String compressionString) {
+        char currentMode = compressionString.charAt(0);
+
+        ArrayList<CharModePair> database = new ArrayList<>();
+
+        ArrayList<CharModePair> alreadyIteratedCharacters = new ArrayList<>();
+
+
+
+
+        for (int i = 0;i < compressionString.length(); i++) {
+
+            alreadyIteratedCharacters.add(new CharModePair(compressionString.charAt(i)));
+            if(compressionString.charAt(i) == currentMode) {
+
+
+                if(!isCharAlreadyIteratedThrough(compressionString.charAt(i), alreadyIteratedCharacters)) {
+                    database.add(new CharModePair(compressionString.charAt(i)));
+                }
+
+
+            } else {
+
+                if(compressionString.length() > 3 && compressionString.charAt(i + 1) != currentMode || compressionString.charAt(i + 2) != currentMode || compressionString.charAt(i + 3) != currentMode) {
+                    currentMode = compressionString.charAt(i);
+                    if(!isCharAlreadyIteratedThrough(compressionString.charAt(i), alreadyIteratedCharacters)) {
+                        database.add(new CharModePair(compressionString.charAt(i)));
+                    }
+                }
+
+
+
+
+            }
+
+        }
+
+        System.out.println("The string size is : " +String.valueOf(database.size()));
+
+        return database.size();
+    }
+
+
+    public static boolean isCharAlreadyIteratedThrough(char currentCharacter, ArrayList<CharModePair> alreadyIteratedCharacters) {
+        boolean isCharAlreadyIteratedThrough = false;
+        for (int j = 0;j < alreadyIteratedCharacters.size();j++) {
+            if(alreadyIteratedCharacters.get(j).getCharacter() == currentCharacter) {
+                isCharAlreadyIteratedThrough = true;
+            }
+        }
+
+        return isCharAlreadyIteratedThrough;
+    }
+
+    private static class CharModePair {
+        char character;
+
+        public CharModePair(char character) {
+            this.character = character;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public void setCharacter(char character) {
+            this.character = character;
+        }
+
     }
 }
---
+++ src/doubly_linked_list/DoublyLinkedList.java
@@ -0,0 +1,146 @@
+package doubly_linked_list;
+
+public class DoublyLinkedList {
+
+    private Node headSentinel;
+    private Node tailSentinel;
+    private int listSize = 0;
+
+    public DoublyLinkedList() {
+        headSentinel = new Node(null,null,null);
+        tailSentinel = new Node(headSentinel,null,null);
+        headSentinel.setNextNode(tailSentinel);
+    }
+
+
+    public void insertAtBeginningOfList(int data) {
+        Node nodeSucceedingHeadSentinel = headSentinel.getNextNode();
+        Node newNode = new Node(headSentinel,nodeSucceedingHeadSentinel,data);
+        headSentinel.setNextNode(newNode);
+        nodeSucceedingHeadSentinel.setPrevNode(newNode);
+        listSize++;
+    }
+
+
+    public void insertAtEndOfList(int data) {
+        Node nodePrecedingTailSentinel = tailSentinel.getPrevNode();
+        Node newNode = new Node(nodePrecedingTailSentinel,tailSentinel,data);
+        tailSentinel.setPrevNode(newNode);
+        nodePrecedingTailSentinel.setNextNode(newNode);
+        listSize++;
+
+    }
+
+
+    public void insertBetweenTwoNodes(int data, Node precedingNode, Node succeedingNode) {
+        Node newNode = new Node(precedingNode,succeedingNode,data);
+        precedingNode.setNextNode(newNode);
+        succeedingNode.setPrevNode(newNode);
+        listSize++;
+    }
+
+    public void findNodeWithParticularValue(int key) {
+        if (isListEmpty()) {
+            System.out.println("The list is empty");
+        } else {
+            Node currentNode = headSentinel.getNextNode();
+            int currentIndex = 0;
+            do {
+                if (currentNode.getData() == key) {
+                    System.out.println(currentIndex);
+                }
+                currentNode = currentNode.getNextNode();
+                currentIndex++;
+            } while (currentNode != tailSentinel);
+
+        }
+    }
+
+
+    public void deleteNode(Node nodeToBeDeleted) {
+        Node precedingNode = nodeToBeDeleted.getPrevNode();
+        Node succeedingNode = nodeToBeDeleted.getNextNode();
+        precedingNode.setNextNode(succeedingNode);
+        succeedingNode.setPrevNode(precedingNode);
+        nodeToBeDeleted.setPrevNode(null);
+        nodeToBeDeleted.setNextNode(null);
+        listSize --;
+    }
+
+
+    public boolean isListEmpty() {
+        return headSentinel.getNextNode() == tailSentinel;
+    }
+
+
+    public void deleteFirstNode() {
+        if (isListEmpty()) {
+            System.out.println("The list is empty");
+        } else {
+            Node nodeToBeDeleted = headSentinel.getNextNode();
+            Node nodeSucceedingNodeToBeDeleted = nodeToBeDeleted.getNextNode();
+            headSentinel.setNextNode(nodeSucceedingNodeToBeDeleted);
+            nodeToBeDeleted.setPrevNode(null);
+            nodeToBeDeleted.setNextNode(null);
+            nodeSucceedingNodeToBeDeleted.setPrevNode(headSentinel);
+            listSize --;
+        }
+    }
+
+
+    public void printList() {
+        if (isListEmpty()) {
+            System.out.println("The list is empty");
+        } else {
+            Node currentNode = headSentinel.getNextNode();
+
+            do {
+                System.out.print(currentNode.getData() + "<-");
+                currentNode = currentNode.getNextNode();
+            } while (currentNode != tailSentinel);
+        }
+
+        System.out.println("\n " + listSize);
+    }
+
+
+    private static class Node {
+        private Node prevNode;
+        private Node nextNode;
+        private Integer data;
+
+        public Node() {
+        }
+
+        public Node(Node prevNode, Node nextNode, Integer data) {
+            this.prevNode = prevNode;
+            this.nextNode = nextNode;
+            this.data = data;
+        }
+
+        public Node getPrevNode() {
+            return prevNode;
+        }
+
+        public void setPrevNode(Node prevNode) {
+            this.prevNode = prevNode;
+        }
+
+        public Node getNextNode() {
+            return nextNode;
+        }
+
+        public void setNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public Integer getData() {
+            return data;
+        }
+
+        public void setData(Integer data) {
+            this.data = data;
+        }
+    }
+
+}
