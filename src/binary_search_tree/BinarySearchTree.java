f21c2d42cff437fdb872ec65e529e67d8041594b
Silas Onyango <silas.onyango93@gmail.com>
2022-04-14 12:24:48 +0300

    Binary Search Tree

---
+++ src/binary_search_tree/BinarySearchTree.java
@@ -0,0 +1,55 @@
+package binary_search_tree;
+
+import org.junit.Test;
+
+public class BinarySearchTree {
+
+    private static class Node {
+        int data;
+        Node leftChild;
+        Node rightChild;
+
+        public Node(int data) {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+
+
+    public Node insertNode(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+
+        if (data < node.data) {
+            node.leftChild = insertNode(node.leftChild, data);
+        } else {
+            node.rightChild = insertNode(node.rightChild, data);
+        }
+        return node;
+    }
+
+    @Test
+    public void insertNode_works_as_expected() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        Node rootNode = null;
+
+        rootNode = binarySearchTree.insertNode(rootNode,8);
+        rootNode = binarySearchTree.insertNode(rootNode,3);
+        rootNode = binarySearchTree.insertNode(rootNode,10);
+        rootNode = binarySearchTree.insertNode(rootNode,1);
+        rootNode = binarySearchTree.insertNode(rootNode,6);
+        rootNode = binarySearchTree.insertNode(rootNode,14);
+        rootNode = binarySearchTree.insertNode(rootNode,4);
+        rootNode = binarySearchTree.insertNode(rootNode,7);
+        rootNode = binarySearchTree.insertNode(rootNode,13);
+
+        assert 8 == rootNode.data;
+        assert 3 == rootNode.leftChild.data;
+        assert 10 == rootNode.rightChild.data;
+        assert 1 == rootNode.leftChild.leftChild.data;
+        assert 6 == rootNode.leftChild.rightChild.data;
+    }
+
+}
