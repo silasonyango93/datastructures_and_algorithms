b6260406de8c6ffecb77ade96095a36e1bf7b75d
Silas Onyango <silas.onyango93@gmail.com>
2022-05-22 18:32:56 +0300

    leetcode#climbStairs()

---
+++ src/leetcode/ClimbStairs.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class ClimbStairs {
+
+    public int climbStairs(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        int [] steps = new int[n + 1];
+        steps[0] = 1;
+        steps[1] = 1;
+
+        for (int i = 2; i <= n; i++ ) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+        return steps[n];
+    }
+
+}
